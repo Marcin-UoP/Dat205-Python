bcdb9a0ac87a7de7516a2764f05fc153156c6bf4
speziall <marcin.roszkowski@students.plymouth.ac.uk>
2015-04-03 11:43:16 +0100

    Update ChatBotTemplate.py

--- Week1-ChatBot/ChatBotTemplate.py
+++ Week1-ChatBot/ChatBotTemplate.py
@@ -1,14 +1,29 @@
-# Knights to was "ni" video reference
-# https://www.youtube.com/watch?v=zIV4poUZAQo
-
-file = open("stop-words.txt")
+# loads and reads all words from txt file
+file = open("stopwords.txt") 
 stopwords = file.readlines()
+stopwords = []
 
 for word in stopwords:
-    next = word.strip()
-    print(next)
+    stopwords.append(word.strip())
 
-while True:
-    input = raw_input("Say something interesting: ")
-    filtered = input.replace(" ni ", " <beep> ")
-    print("Your filtered text was: " + filtered)
+# function that formulates the question 
+def formulate(sentence): 
+    input = raw_input(sentence + " ")
+    words = input.split(' ')
+    for word in words:
+        if word in stopwords:
+            print "stopword: " + word
+            words.remove(word)
+    return ' '.join(words)
+
+user = {} 
+
+# Ask for nick name
+nickNameInput = formulate("Hey bro, I'm a chat bot, please tell me your nick name")
+nickName = nickNameInput.split(' ')[-1]
+user['name'] = nickName
+# Generates answear including user nick name at the end of the line
+print "This is really cool nick name, " + nickName 
+
+input = raw_input("Tell me more... ")
+print("Sorry " + nickName + " but I'm not very clever chat bot :/" )
