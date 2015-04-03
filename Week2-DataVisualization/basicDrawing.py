4763fd9f4a49c895c116fcba0e3889606e32a0a5
speziall <marcin.roszkowski@students.plymouth.ac.uk>
2015-04-03 11:46:47 +0100

    Update basicDrawing.py

--- Week2-DataVisualization/basicDrawing.py
+++ Week2-DataVisualization/basicDrawing.py
@@ -1,35 +1,34 @@
 from graphics import *
 import random
 
-# Read in and print out the data in the data file
-datafile = open("data.txt",'r')
+
+datafile = open("data.txt",'r') # Read in and print out the data in the data file
 bigData = datafile.readlines()
 
-for line in datafile: print(line)
 
-# Do some simple drawing
-window = GraphWin("Visualisation", 600, 600)
+window = GraphWin("Visualisation", 600, 600) # Create a window for all the drawings
 
+for line in bigData: #function to draw different colours and sizes rectangles
+	print(line)
+	box = Rectangle(Point(random.randint(1,600),random.randint(1,600)),Point(300,300))
+	box.setOutline(color_rgb(150,150,150))
+	box.setFill(color_rgb(random.randint(1,200),84,95))
+	box.draw(window)
 
+for line in bigData: #function to draw different colours and sizes circles
+	print(line)
+	ball = Circle(Point(random.randint(1,600),random.randint(1,600)),float(line))
+	ball.setFill(color_rgb(random.randint(1,200),80,0))
+	ball.draw(window)
 
+# for line in bigData:
+# 	print(line)
+# 	line = Line(Point(300,float(line)),Point(300,280))
+# 	line.setWidth(2)
+# 	line.draw(window)
 
-
-
-
-ball = Circle(Point(100,100), 30)
-ball.setFill(color_rgb(255,255,0))
-ball.draw(window)
-
-box = Rectangle(Point(200,50),Point(250,150))
-box.setOutline(color_rgb(255,0,0))
-box.draw(window)
-
-line = Line(Point(200,200),Point(250,280))
-line.setWidth(4)
-line.draw(window)
-
-text = Text(Point(50,200),"Hello")
-text.draw(window)
+# text = Text(Point(50,200),"Hello")
+# text.draw(window)
 
 # Waits until the mouse is clicked before closing the window
 window.getMouse()
